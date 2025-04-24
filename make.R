942736292a46158e730f201c59c3ccedeaf15a69
Natlia Vergara <nvergaramallegas@gmail.com>
2025-04-24 10:11:32 -0400

    init repo

---
+++ make.R
@@ -0,0 +1,31 @@
+#' proyectorepro: A Research Compendium
+#' 
+#' @description 
+#' A paragraph providing a full description of the project and describing each 
+#' step of the workflow.
+#' 
+#' @author Natalia Vergara-Mallegas \email{nvergaramallegas@gmail.com}
+#' 
+#' @date 2025/04/24
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+devtools::install_deps(upgrade = "never")
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+devtools::load_all(here::here())
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("analyses", "script_X.R"))
